d3071d213233ee6bf5f0cc191e2f2d017573174b
kageurarenn <241020@std.hi-joho.ac.jp>
2025-06-20 10:59:58 +0900

    中間課題

---
+++ src/Character.java
@@ -0,0 +1,21 @@
+public abstract class Character implements Creature {
+    String name;
+    int hp;
+    public Character(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+    public abstract void attack(Character target);
+    public  final boolean isAlive() {
+        if (hp > 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public  void showStatus(){
+        System.out.println(name+":HP "+hp);
+    }
+
+
+}
---
+++ src/Slime.java
@@ -0,0 +1,13 @@
+public final class Slime extends Character{
+    public Slime(String name,int hp){
+        super(name,hp);
+        this.name = name;
+        this.hp = hp;
+    }
+    public  void attack(Character target){
+        System.out.println(this.name +"は体当たり攻撃!"+ target.name+"に5のダメージを与えた!");
+        target.hp -= 5;
+    }
+
+
+}
